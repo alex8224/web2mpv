7734ad7b1825b20933cb6e5d0b7a965155367e93
Alex Zhang <alex8224@gmail.com>
2016-08-16 12:34:32 +0800

    add contextmenu for playable video links

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
 chrome.browserAction.onClicked.addListener(function(){
-    chrome.tabs.executeScript(null, {file: "play.js"});
+   chrome.tabs.executeScript(null, {file: "play.js"});
+});
+
+function playInMpv(info, tab) {
+    var playurl = "ytb://" + info.linkUrl;
+    chrome.tabs.executeScript(tab.id, {code:'window.location.href="' + playurl + '"'});
+}
+
+chrome.contextMenus.create({
+    title: "Play in mpv",
+    contexts: ["link"],
+    onclick: function(info, tab) {
+        playInMpv(info, tab);
+    }
 });
